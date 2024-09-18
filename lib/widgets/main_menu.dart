4ef1819c7bc73289288580a12c677b6059d37d9a
Alexander Tometzki <alex.tometzki@icloud.com>
2024-09-18 14:35:05 +0100

    start using import prefixes and creating skeleton

---
+++ lib/widgets/main_menu.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart' as fm;
+
+class MainMenu extends fm.StatefulWidget {
+  const MainMenu({super.key});
+
+  @override
+  fm.State<MainMenu> createState() => _MainMenuState();
+}
+
+class _MainMenuState extends fm.State<MainMenu> {
+  @override
+  fm.Widget build(fm.BuildContext context) {
+    throw UnimplementedError();
+  }
+}
