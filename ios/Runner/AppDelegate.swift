481aa4141d169b5a2ae515b66d02ec39a5c88dd1
Alexander Tometzki <alex.tometzki@icloud.com>
2024-09-19 16:25:01 +0100

    add key to ios file

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import Flutter
 import UIKit
+import GoogleMaps
+import Foundation
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +9,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey(ProcessInfo.processInfo.environment["MAPS_IOS_API_KEY"])
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart' as fm;
 import 'package:restaurantour/app.dart' as app;
 import 'package:flutter_dotenv/flutter_dotenv.dart' as dotenv;
+import 'package:google_maps_place_picker_mb/google_maps_place_picker.dart'
+    as place_picker;
+import 'package:google_maps_flutter/google_maps_flutter.dart' as gm;
+import 'dart:io' show Platform;
 
 Future main() async {
   await dotenv.dotenv.load();
+  if (Platform.isAndroid) {
+    AndroidGoogleMapsFlutter.useAndroidViewSurface = true;
+  }
 
   fm.runApp(
     fm.MaterialApp(
