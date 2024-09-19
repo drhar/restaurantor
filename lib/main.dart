cbc5ff456415dea02c70bb3e68d3f0dc2c963cd3
Alexander Tometzki <alex.tometzki@icloud.com>
2024-09-19 10:35:29 +0100

    trivial color change

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
       title: 'RestauranTour',
       theme: fm.ThemeData().copyWith(
         colorScheme: fm.ColorScheme.fromSeed(
-            seedColor: fm.Color.fromARGB(255, 148, 62, 28)),
+            seedColor: fm.Color.fromARGB(255, 24, 60, 151)),
       ),
       home: const app.RestauranTourApp(),
     ),
